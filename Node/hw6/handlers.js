a4014ac60651bb1a0fb203221dbea363b432a204
Xellaus <Xellaus>
2022-07-08 03:04:55 +0200

    idk which changes in hw6, but here is a commit

--- Node/hw6/handlers.js
+++ Node/hw6/handlers.js
@@ -1,15 +1,27 @@
 const fs = require('fs');
 
 
-const fileWrite = async (reqData,fileName) => {
+const fileWrite = async (reqData, fileName) => {
     return new Promise( (success,fail) => {
-        fs.writeFile(fileName,reqData, (err) => {
+        fs.writeFile(fileName, reqData, (err) => {
             return fail(err);
         });
         return success();
     });
 }
 
+const studenti = async (req, res) => {
+    try {
+        await fileWrite(JSON.stringify(req.body), "./text.txt");
+        console.log('file written!');
+        res.send("File created! with object:" + ' ' + JSON.stringify(req.body));
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+}
+
 module.exports = {
-    fileWrite
+    fileWrite,
+    studenti
 }
--- Node/hw6/index.js
+++ Node/hw6/index.js
@@ -3,20 +3,14 @@ const express = require('express');
 const handlers = require('./handlers.js');
 
 const app = express();
-app.use(express.json())
-app.get("/", (req,res) => {
+app.use(express.json());
+app.get("/", (req, res) => {
     res.send("/home")
 });
-app.post("/", async (req,res) => {
-    try {
-        await handlers.fileWrite(JSON.stringify(req.body),"./text.txt");
-        console.log('file written!')
-        res.send("File created! with object:"+' '+JSON.stringify(req.body))
-    } catch (error) {
-        console.log(error);
-        res.send(error)
-    }
-})
+app.post("/", handlers.studenti);
 
 
 app.listen(8080);
+
+
+//smeni ja funkcijata taka da 
