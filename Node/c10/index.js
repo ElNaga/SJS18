d6506487786e97313c2f21203b5766049c5e9286
Xellaus <Xellaus>
2022-07-12 23:52:13 +0200

    Class 10: express with ejs Calculator WORKING

---
+++ Node/c10/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const handlers = require('./handlers');
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
+app.get('/', (req, res) => {
+  res.render('index'); //views/index.ejs
+});
+
+app.get('/podatoci/:ime', (req, res) => {
+    let data = {
+        ime: req.params.ime,
+        studenti: [
+            {ime: 'Pero', prezime: 'Perovski'},
+            {ime: 'Janko', prezime: 'Jankovski'},
+            {ime: 'Zlatko', prezime: 'Zlatkovski'},
+            {ime: 'Petko', prezime: 'Petkovski'},
+            {ime: 'Stanko', prezime: 'Stankovski'},
+            {ime: 'Ivan', prezime: 'Ivanovski'}
+        ]
+    };
+    res.render('podatoci', data); //views/podatoci.ejs
+  });
+
+app.get('/formular', (req,res) => {
+    res.render('formular') //views/formular.ejs
+});
+
+app.post('/formular-rezultat', (req,res) => {
+    //res.render('formular') //views/formular.ejs
+    //res.send(req.body);
+
+    let data = {
+        ime: req.body.ime,
+        prezime: req.body.prezime
+    };
+    res.render('formular_rezultat',data)
+});
+
+
+app.get('/calculator', (req,res) => {
+    res.render('calculator');
+    
+});
+
+app.post('/calculator-result', async (req, res) => {
+    try {
+        let result = {
+            rezultat: await handlers.calculator(req, res)
+        }
+        res.render('calculator-result',result);
+    } catch (error) {
+        console.log(error);
+    }
+    
+});
+
+app.listen(10000, err => {
+    if (err) return console.log(err);
+    console.log('Server started on port 10000');
+}
+    );
