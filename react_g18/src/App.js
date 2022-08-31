6a1177e20e7a738566f688907191ef83d645c342
Xellaus <Xellaus>
2022-08-31 13:11:12 +0200

    cas3Doma

--- react_g18/src/App.js
+++ react_g18/src/App.js
@@ -1,68 +1,22 @@
-import {useState} from "react"
+import { LifecycleClassComponent } from "./components/LifecycleClassComponent";
+import { useState } from "react";
 
-//import { movies } from "./mock/mockData"
-
-// import { Homework } from "./components/Homework";
-
-import {Input} from "./components/common/Input";
 
 const App = () => {
 
-  const [fieldType, setFieldType] = useState("password")
+  console.log('rendering App')
 
-
-  const [username , setUsername] = useState("")
-  const [password , setPassword] = useState("")
-  const [comment , setComment] = useState("")
-
-  const onMouseDownHandler = () => {
-    setFieldType("text")
-  }
-
-  const onMouseUpHandler = () => {
-    setFieldType("password")
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log({
-      username,password,comment
-    })
-  }
+  const [show, setShow] = useState(true)
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* <Homework movies={movies}/>  */}
-
-      <Input
-      type="text"
-      name="username"
-      placeholder="Enter Username"
-      value={username}
-      onChange = { (event) => { setUsername(event.target.value)}}
-      />
-
-      <Input
-            placeholder="Please enter password"
-            name="password" 
-            type={fieldType} 
-            mouseDown={onMouseDownHandler} 
-            mouseUp={onMouseUpHandler} 
-            value={password}
-            onChange = { (event) => { setPassword(event.target.value)}}
-            />
-
-        <Input 
-        type="text"
-        name = "comment"
-        placeholder="Enter comment"
-        value={comment}
-        onChange = { (event) => { setComment(event.target.value)}}
-        />
-
-        <button type="submit">Submit</button>
-    </form>
+    <div>
+      {show && <LifecycleClassComponent/>}
+      <button
+        onClick={() => setShow((s) => !s)}
+      >Show/Hide</button>
+    </div>
   );
+
 }
 
 export default App
