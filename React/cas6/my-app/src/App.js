428de1a58965dd183ef5798a496be6277ebe2822
Xellaus <Xellaus>
2022-09-02 22:59:34 +0200

    cas6

---
+++ React/cas6/my-app/src/App.js
@@ -0,0 +1,16 @@
+
+
+// to do 
+
+import { Todo } from "./components/Todo";
+import "./css/index.css"
+
+function App() {
+  return (
+    <div >
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/cas6/my-app/src/components/ListTodos.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+export const ListTodos = ({ todos, markAsCompleted, deleteTodo }) => {
+
+
+    const completed = (todos.filter(todo => todo.completed)).length
+
+    const [editId, setEditId] = useState()
+
+    const markAsEditable = (id) => {
+        setEditId(id)
+    }
+
+    const changeText = () => {
+
+    }
+
+    return (
+        <div>
+            <p>Completed : {completed}</p>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id} >
+                        {editId == todo.id ? 
+                            <input value={todo.text}
+                            onChange={ () => changeText(todo.id, )}/>
+                            :
+                            <span className={todo.completed ? "mark-as-done" : ''}>
+                                {todo.text}
+                            </span>
+                        }
+                        <div>
+                            <button
+                                onClick={() => markAsEditable(todo.id)}>
+                                edit
+                            </button>
+
+                            <input
+                                type="checkbox"
+                                value={todo.completed}
+                                checked={todo.completed}
+                                onChange={() => markAsCompleted(todo.id)}
+                            />
+                            <span
+                                className="deleteSpan"
+                                onClick={() => deleteTodo(todo.id)}
+                            >X</span>
+                        </div>
+
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
