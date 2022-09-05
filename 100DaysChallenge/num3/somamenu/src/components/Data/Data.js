c1ac3231f7210cd0ea9b9344994a4e2e30e45526
Xellaus <Xellaus>
2022-09-05 19:05:26 +0200

    AI branch num3DEV

---
+++ 100DaysChallenge/num3/somamenu/src/components/Data/Data.js
@@ -0,0 +1,114 @@
+export const Data = [{
+    "product": "Еспресо",
+    "price": 70,
+    "category": "Кафе",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Макијато",
+    "price": 90,
+    "category": "Кафе",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Мало макијато",
+    "price": 80,
+    "category": "Кафе",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Карамел макијато",
+    "price": 120,
+    "category": "Кафе",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Цеден портокал",
+    "price": 140,
+    "category": "Свежо цедено",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Кока кола",
+    "price": 100,
+    "category": " Безалкохолни пијалоци",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Цеден лимон",
+    "price": 130,
+    "category": "Свежо цедено",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Цеден микс",
+    "price": 170,
+    "category": "Свежо цедено",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Монин лимонада",
+    "price": 130,
+    "category": "Свежо цедено",
+    "description": "Направи сам (јагода, манго, праска, киви)",
+    "linkDes": "",
+    "flag": true
+  },{
+    "product": "Кока кола зеро",
+    "price": 100,
+    "category": "Безалкохолни пијалоци",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Апетисани микс",
+    "price": 180,
+    "category": "Апетисани",
+    "description": "лешници, бадеми, ореви",
+    "linkDes": "",
+    "flag": true
+  },{
+    "product": "Jameson",
+    "price": 230,
+    "category": "Алкохол",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Скопско",
+    "price": 120,
+    "category": "Пиво",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Alexandria Cuve бело",
+    "price": 230,
+    "category": "Вино",
+    "description": "",
+    "linkDes": "",
+    "flag": false
+  },{
+    "product": "Класик",
+    "price": 220,
+    "category": "Сендвичи",
+    "description": "(пилешка сунка, кашкавал,марула,кисели краставички, домат, сос)",
+    "linkDes": "",
+    "flag": true
+  },{
+    "product": "Туна салата",
+    "price": 260,
+    "category": "Салати",
+    "description": "(зелен микс, туна, краставица, шери, црвен кромид, пченка, капари, дресинг)",
+    "linkDes": "",
+    "flag": true
+  }]
+  
