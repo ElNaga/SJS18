9fa0bd53cd74c0ba796ea5b8002ae1a26a0ee2c1
ElNaga <aleksandar.ilijevski.feit@gmail.com>
2022-10-17 19:10:23 +0200

    update soma

--- 100DaysChallenge/num3/somamenu/src/App.js
+++ 100DaysChallenge/num3/somamenu/src/App.js
@@ -1,9 +1,14 @@
 
+import { Route, Routes } from "react-router-dom";
 import { Landing } from "./components/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        {/* <Route path="/coffee" element={}/> */}
+        <Route/>
+      </Routes>
       <Landing />
     </div>
   );
