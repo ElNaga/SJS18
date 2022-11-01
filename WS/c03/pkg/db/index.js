f8c20009f3b79b0eaabc362e0c9d9e92b8e0b00a
Xellaus <Xellaus>
2022-11-01 02:59:26 +0100

    lulu

--- WS/c03/pkg/db/index.js
+++ WS/c03/pkg/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const init = () => {
-    const dsn = 'mongodb+srv://elnaga:Kuchki_3site@cluster0.rvgmx.mongodb.net/baza1?retryWrites=true&w=majority';
+    const dsn = '';
 mongoose.connect (
     dsn,
     err => {
