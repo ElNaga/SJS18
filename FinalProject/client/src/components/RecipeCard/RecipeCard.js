18491b90be8e35ff2d38c5a17fce52fa4802e831
Xellaus <Xellaus>
2022-12-16 16:42:53 +0100

    adding recipe card component

--- FinalProject/client/src/components/RecipeCard/RecipeCard.js
+++ FinalProject/client/src/components/RecipeCard/RecipeCard.js
@@ -1,33 +1,39 @@
 import './recipeCard.css'
 
 import object from "../../mockFolder/mockRecipes.json"
+import timeIco from "../../assets/icon_time.svg"
+import plateIco from "../../assets/icon_plate.svg"
+import favIco from "../../assets/icon_star.svg"
+import arrowsIco from "../../assets/icon_arrows_white.svg"
 const recipe1 = object[0];
 {/* {JSON.stringify(recipe1)} */}
 
 export const RecipeCard = () => {
   return (
     <div className='card--wrapper'>
-        <div>
+        <div className='card--imgAndCategory'>
             <div className='card--tag'>
                 {recipe1.category.toLocaleLowerCase()}
             </div>
-            <img className='card--img' src={recipe1.imgLink} alt="" />
+            <div className='card--ImgDiv'>
+                <img className='card--img' src={recipe1.imgLink} alt="" />
+            </div>
         </div>
         <div className='card--recipeInfo'>
-            <label htmlFor="card--recipeInfoPara">{recipe1.title}</label>
+            <label className="card--title" htmlFor="card--recipeInfoPara">{recipe1.title}</label>
             <p id='card--recipeInfoPara'>{recipe1.shortDesc}</p>
         </div>
         <div className='card--footer'>
             <div className='card--footerIcons'>
-                <img className='card--icon' src="" alt="" />
+                <img className='card--icon' src={timeIco} alt="" />
                 <p className='card--info'>{recipe1.preparationTime} min</p>
-                <img className='card--icon' src="" alt="" />
-                <p className='card--info'>{recipe1.numPeople}</p>
-                <img className='card--icon' src="" alt="" />
+                <img className='card--icon' src={plateIco} alt="" />
+                <p className='card--info'>{recipe1.numPeople > 1 ? `${recipe1.numPeople} persons` : `${recipe1.numPeople} person` }</p>
+                <img className='card--icon' src={favIco} alt="" />
                 <p className='card--info'>{recipe1.numberFavorited}</p>
             </div>
             <div className='card--footerButton'>
-                <img src="" alt="" />
+                <img src={arrowsIco} alt="" />
             </div>
         </div>
     </div>
