1f9c396d42fa1f70031f47cde917ac4fae1189c9
Xellaus <Xellaus>
2023-02-12 21:35:51 +0100

    adding Fly deployment .toml and Dockerfile

--- FinalProject/Server/proxy/index.js
+++ FinalProject/Server/proxy/index.js
@@ -1,7 +1,7 @@
 
 const config = require("../../pkg/config");
 const express = require('express');
-const proxy = require('express-http-proxy');
+const proxy = require('express-http-proxy'); 
 
 const app = express();
 
@@ -48,6 +48,28 @@ app.use(
     )
 );
 
+
+
+
+
+const PORT = process.env.PORT || config.get("services").proxy.port;
+
+// PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION / PRODUCTION /
+
+// app.use(
+// '/',
+//     express.static(`${__dirname}/../../client/build`)
+// );
+
+// app.listen(PORT, "0.0.0.0", err => {
+//     if (err) {
+//         return console.log(err);
+//     }
+//     console.log(`[Proxy] service succesfully started on port ${PORT}`);
+// })
+
+// DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT / DEVELOPEMENT
+
 app.use(
 '/',
     proxy(
@@ -57,8 +79,6 @@ app.use(
         }
     )
 );
-
-const PORT = process.env.PORT || config.get("services").proxy.port;
 
 app.listen(PORT, err => {
     if (err) {
--- FinalProject/client/src/components/Login/Login.js
+++ FinalProject/client/src/components/Login/Login.js
@@ -10,6 +10,8 @@ export const Login = () => {
     const dispatch = useDispatch();
 
     const [token, setToken] = useState('testing state Token text');
+    
+    const [invalidInfo, setInvalidInfo] = useState(false);
 
     let navigate = useNavigate();
     const routeChangeHome = () =>{ 
@@ -44,11 +46,12 @@ export const Login = () => {
                 }
             });
             let rez = await response.json();
-            console.log(rez);
+            console.log('this is result',rez);
             localStorage.setItem("token", rez.token);
             return rez;
 
         } catch (err) {
+            setInvalidInfo(true);
             throw err;// throws to logMeIn  func
         }
     };
@@ -81,7 +84,16 @@ export const Login = () => {
                         <p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
                     </div>
                     <div className='centerContent__right__wrapper'>
+
                         <div className='centerContent__right'>
+                        {invalidInfo ? <div style={{color: 'red' }}>
+                            Bad Login Information!
+                            <br />
+                            <br />
+                            Try again.
+                            <br />
+                            <br />
+                        </div> : null}
                             <label className='emailLabel' htmlFor="email"  >Email</label>
                             <input className='emailInput' type="email" placeholder='user@domain.com' name='email' value={loginData.email} onChange={dataChange}/>
                             <label className='passwordLabel' htmlFor="password">Password</label>
